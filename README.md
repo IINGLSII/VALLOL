a11d0d9c98ee7b1f746a6f924c71319776286673
IINGLSII <nickolassimons367@gmail.com>
2024-03-19 01:38:45 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# VALLOL
+
+Developed with Unreal Engine 5
+CS415 Term Project
---
+++ Source/VALLOL/VALLOL.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "VALLOL.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VALLOL, "VALLOL" );
